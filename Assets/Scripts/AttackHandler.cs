a86fcf944c6c8fdfb0ab688c50afc9e55c375163
Jolo V <us3r100@gmail.com>
2022-06-26 15:52:23 -0500

    Added heavy punch, started on collisions

--- Assets/Scripts/AttackHandler.cs
+++ Assets/Scripts/AttackHandler.cs
@@ -7,27 +7,58 @@ public class AttackHandler : MonoBehaviour
 {
     Animator anim;
     Rigidbody2D rb;
+    Transform hitboxPoint;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        hitboxPoint = transform.Find("HitboxPoint").transform;
     }
 
     void ListenForAttack()
     {
+        bool attacked = false;
         if (Input.GetButtonDown("Light Punch"))
         {
             anim.SetTrigger("Light Punch");
+            attacked = true;
         }
         if (Input.GetButtonDown("Medium Punch"))
         {
             anim.SetTrigger("Medium Punch");
+            attacked = true;
         }
         if (Input.GetButtonDown("Heavy Punch"))
         {
-            Debug.Log("Heavy Punch");
             anim.SetTrigger("Heavy Punch");
+            attacked = true;
+        }
+        if (attacked)
+        {
+            // The animation handles setting to false after the attack is done.
+            hitboxPoint.gameObject.SetActive(true);
+        }
+    }
+
+    // Called using Animation Events, spawns a hitbox and checks for collisions.
+    public void CheckForCollisionDuringAttack()
+    {
+        Collider2D[] hitColliders = Physics2D.OverlapBoxAll(hitboxPoint.position, new Vector2(0.5f, 0.5f), 0);
+        foreach (Collider2D hitCollider in hitColliders)
+        {
+            //TODO: Check if the hitCollider is the opponent.
+            // if (hitCollider.gameObject.tag == "Enemy")
+            // {
+            //     hitCollider.gameObject.GetComponent<Enemy>().TakeDamage(10);
+            // }
+        }
+    }
+
+    private void OnDrawGizmos() {
+        if (hitboxPoint != null)
+        {
+            Gizmos.DrawWireCube(hitboxPoint.position, new Vector3(0.5f, 0.5f, 0));
         }
     }
 
