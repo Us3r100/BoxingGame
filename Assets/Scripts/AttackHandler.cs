b276b935d9b5a5ef1151e7cdd21d7ea16a36002c
Jolo V <us3r100@gmail.com>
2022-06-29 01:38:28 -0500

    Added animation and control for p2

--- Assets/Scripts/AttackHandler.cs
+++ Assets/Scripts/AttackHandler.cs
@@ -7,58 +7,36 @@ public class AttackHandler : MonoBehaviour
 {
     Animator anim;
     Rigidbody2D rb;
-    Transform hitboxPoint;
+    [SerializeField]
+    string lightPunchInput = "Light Punch";
+    [SerializeField]
+    string mediumPunchInput = "Medium Punch";
+    [SerializeField]
+    string heavyPunchInput = "Heavy Punch";
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
-        hitboxPoint = transform.Find("HitboxPoint").transform;
     }
 
     void ListenForAttack()
     {
         bool attacked = false;
-        if (Input.GetButtonDown("Light Punch"))
+        if (Input.GetButtonDown(lightPunchInput))
         {
             anim.SetTrigger("Light Punch");
             attacked = true;
         }
-        if (Input.GetButtonDown("Medium Punch"))
+        if (Input.GetButtonDown(mediumPunchInput))
         {
             anim.SetTrigger("Medium Punch");
             attacked = true;
         }
-        if (Input.GetButtonDown("Heavy Punch"))
+        if (Input.GetButtonDown(heavyPunchInput))
         {
             anim.SetTrigger("Heavy Punch");
             attacked = true;
-        }
-        if (attacked)
-        {
-            // The animation handles setting to false after the attack is done.
-            hitboxPoint.gameObject.SetActive(true);
-        }
-    }
-
-    // Called using Animation Events, spawns a hitbox and checks for collisions.
-    public void CheckForCollisionDuringAttack()
-    {
-        Collider2D[] hitColliders = Physics2D.OverlapBoxAll(hitboxPoint.position, new Vector2(0.5f, 0.5f), 0);
-        foreach (Collider2D hitCollider in hitColliders)
-        {
-            //TODO: Check if the hitCollider is the opponent.
-            // if (hitCollider.gameObject.tag == "Enemy")
-            // {
-            //     hitCollider.gameObject.GetComponent<Enemy>().TakeDamage(10);
-            // }
-        }
-    }
-
-    private void OnDrawGizmos() {
-        if (hitboxPoint != null)
-        {
-            Gizmos.DrawWireCube(hitboxPoint.position, new Vector3(0.5f, 0.5f, 0));
         }
     }
 
